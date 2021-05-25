43447abf342ebab48e6b9de9fa6f23b37895dd01
Dominik Olczyk <dominik@cryptosphere-systems.com>
2021-05-25 16:35:31 +0200

    Add README example

--- README.md
+++ README.md
@@ -2,13 +2,25 @@
 
 Simple implementation of the Tari Protocol wallet gRPC Client.
 
+## Installation
+You can install this package using pip:
 
-## Guide
-
-To start generate proto files with command:
-> chmod +x build_protos.sh && ./build_protos.sh
+> pip install git+git://github.com/Domol/tari-python-wallet-grpc-client
 
 You can then use Client class to make calls to Tari wallet RPC.
-Example code is in example.py
+
+## Example Usage
+Here is a simple code that prints wallet balance:
+```{python}
+from tari_python import Client
+
+wallet_address = "localhost:18143"
+wallet = Client(wallet_address)
+
+response = wallet.get_balance()
+balance = int(response["available_balance"]) / 1e6
+print(f"Wallet balance is {balance}")
+
+```
 
 
--- setup.py
+++ setup.py
@@ -1,39 +1,4 @@
-import re
-
-from distutils.command.sdist import sdist as sdist_orig
-from distutils.errors import DistutilsExecError
-
 from setuptools import setup
-
-
-class sdist(sdist_orig):
-    def __init__(self):
-        try:
-            self.info("Generating proto files...")
-            self.spawn(
-                [
-                    "python -m grpc_tools.protoc --proto_path=protos --python_out=$(pwd) --grpc_python_out=$(pwd) protos/*.proto"
-                ]
-            )
-            script_names = [
-                "tari_python/types_pb2.py",
-                "tari_python/types_pb2_grpc.py",
-                "tari_python/wallet_pb2.py",
-                "tari_python/wallet_pb2_grpc.py",
-            ]
-            # Make pb2 imports in generated scripts relative
-            for script in script_names:
-                with open(script, "r+") as file:
-                    code = file.read()
-                    file.seek(0)
-                    file.write(
-                        re.sub(r"(import .+_pb2.*)", "from . \\1", code)
-                    )
-                    file.truncate()
-            # self.spawn(["sed -i -r 's/^import (.+_pb2.*)/from . import \1/g' tari_python/*_pb2*.py"])
-        except DistutilsExecError:
-            self.warn("Generation of proto files failed.")
-
 
 setup(
     name="tari-python",
@@ -44,7 +9,6 @@ setup(
     author_email="dominik.olczyk@gmail.com",
     license="BSD 2-clause",
     packages=["tari_python"],
-    cmdclass={"sdist": sdist},
     install_requires=["grpcio>=1.37.1", "grpcio-tools>=1.37.1"],
     classifiers=[
         "Development Status :: 1 - Planning",
